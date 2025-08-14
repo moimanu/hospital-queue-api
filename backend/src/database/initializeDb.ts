c4e49b160f8fb10feb276d4851dfe91fae95a26f
Moisés Emanuel <moisesemanuelreisdacruz@hotmail.com>
2025-08-13 22:50:14 -0300

    Criando sincronização para dados de testes ao inicializar

--- backend/src/database/initializeDb.ts
+++ backend/src/database/initializeDb.ts
@@ -1,4 +1,5 @@
 import { db } from './db';
+import { syncRealtimeDatabase } from "../services/firebaseSyncService";
 
 // Utilitário para converter Date -> 'YYYY-MM-DD HH:mm:ss'
 function formatDate(date: Date): string {
@@ -100,6 +101,9 @@ export function initializeDatabase() {
     console.log("\n📊 Inserindo 100 registros...");
     db.exec(insertStatements);
     populateLastDays();
+
+    syncRealtimeDatabase().catch(console.error);
+    console.log("\n🔥 Sincronização concluída...");
   } else {
     console.log("\n👍 A tabela já contém registros. Nenhuma inserção necessária.");
   }
@@ -118,5 +122,5 @@ function populateLastDays() {
   `;
 
   db.exec(query);
-  console.log('\n📅 Tabela LastDays foi preenchida com a quantidade de registros por data.');
+  console.log('\n📅 Tabela LastDays foi preenchida com a quantidade de registros por data.');  
 }
