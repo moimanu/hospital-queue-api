79c4f02f9587c5387376c7f63930dc0db1e2478e
Moisés Emanuel <moisesemanuelreisdacruz@hotmail.com>
2025-09-22 01:15:11 -0300

    Realizando teste de conexão entre as hospedagens (CORS)

--- backend/src/sse/publicStream.ts
+++ backend/src/sse/publicStream.ts
@@ -7,6 +7,9 @@ export function streamHandler(req: Request, res: Response) {
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
+
+  res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL || "*");
+    
   res.flushHeaders();
 
   res.write(`data: ${JSON.stringify(cacheService.getCache())}\n\n`);
